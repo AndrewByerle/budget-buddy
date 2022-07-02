973d6cb77ef08ee85cd673f8d26851c65249b40f
Andrew Byerle <andrewbyerle@INTERNMewByerle.lan>
2022-07-02 13:40:10 -0400

    transaction card styling

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -38,30 +38,36 @@ const formatDate = (date: Date) => {
   <button @click="clearTransactions">clear</button>
   <button @click="editTransactions">edit</button>
 
-  <div class="transaction">
-    <div class="card">
-      <div class="row">
-        <p>Description</p>
-        <input type="string" v-model="description" placeholder="Description" />
-      </div>
-      <div class="row">
-        <p>Category</p>
-        <select v-model="categorySelected">
-          <option :value="undefined">Please select one</option>
-          <option v-for="category in categories" :value="category">
-            {{ category.name }}
-          </option>
-        </select>
-      </div>
-      <div>
+  <div class="transaction-card">
+    <div class="row-center">
+      <div class="container">
         <div class="row">
-          <p>Amount</p>
-          <input type="number" v-model="amount" placeholder="$Amount" />
+          <p>Description</p>
+          <input
+            type="string"
+            v-model="description"
+            placeholder="Description"
+          />
         </div>
-      </div>
-      <div class="row">
-        <p>date</p>
-        <Datepicker v-model="date"></Datepicker>
+        <div class="row">
+          <p>Category</p>
+          <select v-model="categorySelected">
+            <option :value="undefined">Please select one</option>
+            <option v-for="category in categories" :value="category">
+              {{ category.name }}
+            </option>
+          </select>
+        </div>
+        <div>
+          <div class="row">
+            <p>Amount</p>
+            <input type="number" v-model="amount" placeholder="$Amount" />
+          </div>
+        </div>
+        <div class="row">
+          <p>Date</p>
+          <Datepicker v-model="date" style="width: 44%"></Datepicker>
+        </div>
       </div>
     </div>
   </div>
@@ -70,16 +76,27 @@ const formatDate = (date: Date) => {
 </template>
 
 <style>
-.card {
-  border: 1px solid rgb(232, 240, 243);
-  margin: 20px;
+input {
+  width: 42%;
 }
 
-.transaction {
+select {
+  width: 44%;
+}
+.container {
+  width: 50%;
+}
+.row-center {
+  margin: 20px;
+  border: 1px solid rgb(199, 207, 209);
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+}
+.transaction-card {
   background-color: white;
   border: 1px solid #eee;
   border-radius: 10px;
-  margin-top: 3%;
   margin-left: 3%;
   margin-right: 3%;
   box-shadow: 2px 2px 5px rgb(188, 231, 218);
@@ -89,8 +106,8 @@ const formatDate = (date: Date) => {
 }
 .row {
   display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-between;
+  padding: 2%;
 }
 .add-transaction-button {
   background: inherit;
