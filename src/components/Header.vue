a3f1f9cc5a77cd8f55669a360b5585c3ffe577e8
Andrew Byerle <andran@live.unc.edu>
2022-11-03 23:41:54 -0400

    date picker

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
 import { useAllowance } from "@/composables/allowance";
-import { ref } from "vue";
+import { computed } from "@vue/reactivity";
+import { ref, watch } from "vue";
 import ProfileCard from "./profile/ProfileCard.vue";
 import ProfileIcon from "./profile/ProfileIcon.vue";
 
-const month = new Date().toLocaleString("en-US", { month: "long" });
+const selectedMonth = ref();
+const month = ref(new Date().toLocaleString("en-US", { month: "long" }));
 const { remaining } = useAllowance();
+
+const getMonthName = (monthNumber: number) => {
+  const date = new Date();
+  date.setMonth(monthNumber);
+
+  return date.toLocaleString("en-US", { month: "long" });
+};
+
+watch(selectedMonth, () => {
+  console.log(selectedMonth.value);
+  selectedMonth.value !== null ??
+    (month.value = getMonthName(selectedMonth.value.month));
+});
 </script>
 
 <template>
   <ProfileCard />
   <ProfileIcon />
   <div class="header">
-    <h1>{{ month }}</h1>
+    <div class="month">
+      <h1>{{ month }}</h1>
+      <div class="monthPicker">
+        <Datepicker v-model="selectedMonth" monthPicker />
+      </div>
+    </div>
     <div class="monthly-allowance">
       <p>${{ remaining }}</p>
     </div>
@@ -20,6 +40,18 @@ const { remaining } = useAllowance();
 </template>
 
 <style scoped>
+.month {
+  position: relative;
+}
+.monthPicker {
+  box-shadow: 0 0 6px #1976d2;
+  color: #1976d2;
+  opacity: 0;
+  position: absolute;
+  top: 15px;
+  z-index: 1;
+  display: transparent;
+}
 .header {
   display: flex;
   justify-content: space-around;
