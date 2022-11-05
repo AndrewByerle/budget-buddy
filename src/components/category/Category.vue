aaf63bb7e289fceda50bf200ff1c5e1d9ab2fde3
Andrew Byerle <andran@live.unc.edu>
2022-11-05 13:57:16 -0400

    cat expense 2 decimal fix

--- src/components/category/Category.vue
+++ src/components/category/Category.vue
@@ -29,7 +29,7 @@ const isEditActive = ref(false);
       </div>
     </div>
     <div class="item">
-      <p>${{ category.expense }}</p>
+      <p>${{ category.expense.toFixed(2) }}</p>
     </div>
   </div>
 </template>
